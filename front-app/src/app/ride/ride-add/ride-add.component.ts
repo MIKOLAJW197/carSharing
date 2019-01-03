efcf12fd8aa20988f38b1f3923aa635e2023add1
Mikołaj Wolicki <wolicki.mikolaj@gmail.com>
2019-01-03 19:15:11 +0100

    Dodaj szkielet dodawania przejazdu - TOCHECK

--- front-app/src/app/ride/ride-add/ride-add.component.ts
+++ front-app/src/app/ride/ride-add/ride-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-ride-add',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RideAddComponent implements OnInit {
 
-  constructor() { }
+  rideForm: FormGroup;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    // const text = this.textForm.get('text').value;
+    // this.orginalString=text;
+    // const transformation = this.transformationList.map(el => el.key).join(',');
+    // const request = AppComponent.toNewRequest(text, transformation);
+    // this.apiService.getTransformedText(request).subscribe(resp => {
+    //   this.transformedString=resp.transformed
+    //   this.openAddFileDialog();
+    // });
+  }
+
+  private initForm() {
+    this.rideForm = new FormGroup({
+      dataRozpoczecia: new FormControl(),
+      dataZakonczenia: new FormControl(),
+      dystans: new FormControl(),
+      lokalizacjaPoczatkowa: new FormControl(),
+      lokalizacjaKoncowa: new FormControl(),
+      uzytkownikMail: new FormControl(),
+      cennikOdKiedy: new FormControl(),
+      samochodNrRejestracyjny: new FormControl()
+    });
+  }
 }
