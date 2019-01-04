5312a34ba16a5e6e67d8e9f578ad47ccce7082c2
Mikołaj Wolicki <wolicki.mikolaj@gmail.com>
2019-01-04 21:22:33 +0100

    Dodaj szkielet dodawania doładowania

--- front-app/src/app/top-up/top-up-add/top-up-add.component.ts
+++ front-app/src/app/top-up/top-up-add/top-up-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-top-up-add',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopUpAddComponent implements OnInit {
 
-  constructor() { }
+  topupForm: FormGroup;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    // const text = this.textForm.get('text').value;
+    // this.orginalString=text;
+    // const transformation = this.transformationList.map(el => el.key).join(',');
+    // const request = AppComponent.toNewRequest(text, transformation);
+    // this.apiService.getTransformedText(request).subscribe(resp => {
+    //   this.transformedString=resp.transformed
+    //   this.openAddFileDialog();
+    // });
+  }
+
+  private initForm() {
+    this.topupForm = new FormGroup({
+      data: new FormControl(),
+      kwota: new FormControl(),
+      sposobPlatnosci: new FormControl(),
+      uzytkownikMail: new FormControl()
+    });
+  }
 }
