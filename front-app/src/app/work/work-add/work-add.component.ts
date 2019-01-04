c85bc972b55604970e375523e67e36e39d2ffdd9
Mikołaj Wolicki <wolicki.mikolaj@gmail.com>
2019-01-04 21:48:13 +0100

    Dodaj szkielet dodawania prac technicznych

--- front-app/src/app/work/work-add/work-add.component.ts
+++ front-app/src/app/work/work-add/work-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-work-add',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkAddComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  workForm: FormGroup;
+
+  constructor() {
   }
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    // const text = this.textForm.get('text').value;
+    // this.orginalString=text;
+    // const transformation = this.transformationList.map(el => el.key).join(',');
+    // const request = AppComponent.toNewRequest(text, transformation);
+    // this.apiService.getTransformedText(request).subscribe(resp => {
+    //   this.transformedString=resp.transformed
+    //   this.openAddFileDialog();
+    // });
+  }
+
+  private initForm() {
+    this.workForm = new FormGroup({
+      id: new FormControl(),
+      coRobione: new FormControl(),
+      odKiedy: new FormControl(),
+      doKiedy: new FormControl(),
+      bazaLokalizacja: new FormControl(),
+      samochodNrRejestracyjny: new FormControl()
+    });
+  }
 }
